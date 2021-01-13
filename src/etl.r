8bc3297bd7603e84540f9bdacff3f92314543ec7
JosephBui21 <43527133+JosephBui21@users.noreply.github.com>
2021-01-13 15:22:02 -0800

    delete etl.r

--- src/etl.r
+++
@@ -1,3 +0,0 @@
-create_synthetic_data <- function(data, vars, samples_per_cond, n_diffexp, output_file) {
-    data = generateSyntheticData(dataset = data, n.vars = vars, samples.per.cond = samples_per_cond, n.diffexp = n_diffexp, output.file = output_file)
-}
