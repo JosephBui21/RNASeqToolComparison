3b76d9648364d155ece1985d58ff9bc8d1ceca05
job017 <job017@job017-19686.pods.job017.svc.cluster.local>
2021-01-13 17:12:06 -0800

    added Rscript & etl.py

--- src/etl.py
+++ src/etl.py
@@ -1,5 +1,5 @@
 import os
 
 def run_rscript(data, n_vars, samples_per_cond, n_diffexp, output_file):
-    os.system("Rscript src/syntheticDataFunc.R ")# + data + ' ' + str(n_vars) + ' ' + str(samples_per_cond) + ' ' + str(n_diffexp) + ' ' + output_file)
+    os.system("Rscript src/syntheticDataFunc.R " + data + ' ' + str(n_vars) + ' ' + str(samples_per_cond) + ' ' + str(n_diffexp) + ' ' + output_file)
     return
--- src/syntheticDataFunc.R
+++ src/syntheticDataFunc.R
@@ -3,13 +3,4 @@ myArgs <- commandArgs(trailingOnly = TRUE)
 
 library('compcodeR')
 
-data = myArgs[1]
-n_vars = myArgs[2]
-samples_per_cond = myArgs[3]
-n_diffexp = myArgs[4]
-output_file = myArgs[5]
-
-
-create_synthetic_data <- function(data, n_vars, samples_per_cond, n_diffexp, output_file) {
-    data = generateSyntheticData(dataset = data, n.vars = vars, samples.per.cond = samples_per_cond, n.diffexp = n_diffexp, output.file = output_file)
-}
+generateSyntheticData(dataset = myArgs[1], n.vars = strtoi(myArgs[2]), samples.per.cond = strtoi(myArgs[3]), n.diffexp = strtoi(myArgs[4]), output.file = myArgs[5])
