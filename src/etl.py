b1da68fdb69cc8edfc1948d5ec5adbe583a73126
job017 <job017@job017-9552.pods.job017.svc.cluster.local>
2021-01-15 14:31:39 -0800

    tried things TA recommended

--- src/etl.py
+++ src/etl.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 def run_rscript(data, n_vars, samples_per_cond, n_diffexp, output_file):
-    os.system("Rscript src/syntheticDataFunc.R " + data + ' ' + str(n_vars) + ' ' + str(samples_per_cond) + ' ' + str(n_diffexp) + ' ' + output_file)
+    os.system("/opt/conda/bin/Rscript src/syntheticDataFunc.R " + data + ' ' + str(n_vars) + ' ' + str(samples_per_cond) + ' ' + str(n_diffexp) + ' ' + output_file)
     return
 
 def run_rscript_test(string):
--- src/test.R
+++ src/test.R
@@ -1,3 +1,4 @@
+
 myArgs <- commandArgs(trailingOnly = TRUE)
 
 print(myArgs[1])
