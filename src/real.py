405bf8b06bc7aa07d7c3020cd33238c7b01564a6
Luigi <46508936+iLuigi98@users.noreply.github.com>
2021-03-07 14:46:46 -0800

    real-updated

--- src/real.py
+++ src/real.py
@@ -8,3 +8,7 @@ def run_real_rscript(real_data_dir):
 def run_real_diff_exp_rscript(in_dir, real_data, tool, tool_rmd, out_dir):
     os.system("/opt/conda/envs/r-bio/bin/Rscript src/real_diff_exp.R " + in_dir + ' ' + real_data + ' ' + tool + ' ' + tool_rmd + ' ' + out_dir)
     return
+
+def run_comparison_real(dataset, tool1, tool2, tool3, tool4, tool5, tool6, out_dir):
+    os.system("/opt/conda/envs/r-bio/bin/Rscript src/compareReal.R " + dataset + ' ' + tool1 + ' ' + tool2 + ' ' + tool3 + ' ' + tool4 + ' ' + tool5 + ' ' + tool6 + ' ' + out_dir)
+    return
--- src/real_diff_exp.R
+++ src/real_diff_exp.R
@@ -1,9 +1,8 @@
 #command arguments from config/analysis-params.json which specifies what tool is being used & in/out file directories
 myArgs <- commandArgs(trailingOnly = TRUE)
 library('compcodeR')
-#library('baySeq')
-#library('ABSSeq')
-#library("PoissonSeq")
+library('ABSSeq')
+library("PoissonSeq")
 
 indir <- myArgs[1]
 data <- myArgs[2]
@@ -26,69 +25,46 @@ if (tool == 'voom.limma' | tool == 'ttest' | tool == 'NOISeq') {
     runDiffExp(data.file = file.path(indir, data), result.extent = tool, Rmdfunction = rmdFunc, output.directory = file.path(outdir), norm.method = "TMM")
     }
 
-# if (tool == 'PoissonSeq') {
-#     for (sample in sample_no) {
-#         for (v in versions) {
-#             file <- paste(data, sample, paste(v,'.rds',sep=""), sep="_")
-#             #reformat compcode data to work for PoissonSeq
-#             path <- paste("~/RNASeqToolComparison",indir,data,file, sep="/")
-#             temp_data <- readRDS(path)
-#             n <- temp_data@count.matrix
-#             y <- c(1,1,1,1,1,2,2,2,2,2)
-#             type <-'twoclass'
-#             pair <- FALSE
-#             transformed_data <- list(n=n, y=y, type=type, pair=pair)
-#             pois_res <- PS.Main(transformed_data)
-#             ordered_pois_res <- pois_res[order(pois_res['gname'], decreasing=FALSE),]
+if (tool == 'PoissonSeq') {
+    #reformat compcode data to work for PoissonSeq
+    outdir <- myArgs[6]
+    path <- paste("~/RNASeqToolComparison", indir, data, sep="/")
+    temp_data <- readRDS(path)
+                         
+    n <- temp_data@count.matrix
+    labels <- temp_data@sample.annotations['condition']
+    labels[which(labels[,1] == 'Control', arr.ind=TRUE), 2] <- 1
+    labels[which(labels[,1] != 'Control', arr.ind=TRUE), 2] <- 2
+    y <- labels$V2
+    type <-'twoclass'
+    pair <- FALSE
+    transformed_data <- list(n=n, y=y, type=type, pair=pair)
+    pois_res <- PS.Main(transformed_data)
+    ordered_pois_res <- pois_res[order(pois_res['gname'], decreasing=FALSE),]
+    
+    
+    filename <- paste(substr(outdir, 10, nchar(outdir)),"_",tool,".rds",sep="")
+    out <- paste("~/RNASeqToolComparison", outdir, filename,  sep="/")
+    saveRDS(ordered_pois_res, out)
+}
 
-#             labels <- matrix(c(temp_data@variable.annotations$differential.expression, c(1:NROW(n))), ncol=2)
-#             colnames(labels) <- c("actual", "gene_number")
-#             results <- merge(x=labels, y=pois_res, by.x="gene_number", by.y="gname", all.x=TRUE)
-#             result_df <- as.data.frame(results)
-
-#             result_df$prediction <- ifelse(result_df$pval < 0.05, 1, 0)
-
-#             result_df$prediction[is.na(result_df$prediction)] <- 0
-
-#             result_df$dif <- abs(result_df$actual - result_df$prediction)
-
-
-#             filename <- paste(data,"_",sample,"_",v,"_",tool,".rds",sep="")
-#             out <- paste("~/RNASeqToolComparison", outdir, filename,  sep="/")
-#             saveRDS(result_df, out)
-#         }
-#     }
-# }
-
-# if (tool == 'ABSSeq') {
-#     for (sample in sample_no) {
-#         for (v in versions) {
-#             file <- paste(data, sample, paste(v,'.rds',sep=""), sep="_")
-#             #reformat compcode data to work for PoissonSeq
-#             path <- paste("~/RNASeqToolComparison",indir,data,file,sep="/")
-#             temp_data <- readRDS(path)
-#             groups <- c(1,1,1,1,1,2,2,2,2,2)
-#             absdata <- ABSDataSet(temp_data@count.matrix, groups)
-
-#             obj <- ABSSeq(absdata, useaFold=TRUE)
-#             abs_res <- results(obj,c("Amean","Bmean","foldChange","pvalue","adj.pvalue"))
-
-#             labels <- matrix(c(temp_data@variable.annotations$differential.expression, c(1:NROW(temp_data@count.matrix))), ncol=2)
-#             colnames(labels) <- c("actual", "gene_number")
-#             abs_res <- cbind(gene = rownames(abs_res), abs_res)
-#             rownames(abs_res) <- 1:nrow(abs_res)
-#             abs_res <- cbind(gene_number = rownames(abs_res), abs_res)
-#             rownames(abs_res) <- 1:nrow(abs_res)
-#             results <- merge(x=labels, y=abs_res, by="gene_number",all.x=TRUE)
-#             result_df <- as.data.frame(results)
-#             result_df$prediction <- ifelse(result_df$adj.pval < 0.05, 1, 0)
-#             result_df$prediction[is.na(result_df$prediction)] <- 0
-#             result_df$dif <- abs(result_df$actual - result_df$prediction)
-#             result_df <- result_df[c("gene","Amean","Bmean","foldChange","pvalue","adj.pvalue","actual","prediction","dif")]
-
-#             filename <- paste(data,"_",sample,"_",v,"_",tool,".rds",sep="")
-#             out <- paste("~/RNASeqToolComparison", outdir, filename,  sep="/")
-#             saveRDS(result_df, out)
-#         }
-#     }
-# }
+if (tool == 'ABSSeq') {
+    #reformat compcode data to work for PoissonSeq
+    outdir <- myArgs[6]
+    path <- paste("~/RNASeqToolComparison", indir, data, sep="/")
+    temp_data <- readRDS(path)
+                         
+    labels <- temp_data@sample.annotations['condition']
+    labels[which(labels[,1] == 'Control', arr.ind=TRUE), 2] <- 1
+    labels[which(labels[,1] != 'Control', arr.ind=TRUE), 2] <- 2
+    groups <- labels$V2
+    
+    absdata <- ABSDataSet(temp_data@count.matrix, groups)
+    obj <- ABSSeq(absdata, useaFold=TRUE)
+    abs_res <- results(obj,c("Amean","Bmean","foldChange","pvalue","adj.pvalue"))
+    abs_res <- as.data.frame(abs_res)
+                   
+    filename <- paste(substr(outdir, 10, nchar(outdir)),"_",tool,".rds",sep="")      
+    out <- paste("~/RNASeqToolComparison", outdir, filename,  sep="/")
+    saveRDS(abs_res, out)
+}
